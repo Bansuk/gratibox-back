4a494401b8d4c3a491308053e9faf0a161f058b6
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-11-20 10:09:25 -0300

    Fix sign-up test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import signUp from './Controllers/signUp.js';
+import SignUp from './Controllers/SignUp.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/sign-up', signUp);
+app.post('/sign-up', SignUp);
 
 export default app;
